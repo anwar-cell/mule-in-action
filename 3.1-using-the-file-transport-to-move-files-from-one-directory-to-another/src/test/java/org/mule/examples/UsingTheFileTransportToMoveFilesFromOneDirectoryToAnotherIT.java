9c72b13d04a1cd1e0f399addd78d2ee50a45d083
Miroslav Rusin <miroslav.rusin@hotovo.org>
2014-11-24 13:16:16 +0100

    test code cleanup

--- 3.1-using-the-file-transport-to-move-files-from-one-directory-to-another/src/test/java/org/mule/examples/UsingTheFileTransportToMoveFilesFromOneDirectoryToAnotherIT.java
+++ 3.1-using-the-file-transport-to-move-files-from-one-directory-to-another/src/test/java/org/mule/examples/UsingTheFileTransportToMoveFilesFromOneDirectoryToAnotherIT.java
@@ -8,8 +8,6 @@ import java.util.concurrent.CountDownLatch;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mule.api.context.notification.EndpointMessageNotificationListener;
-import org.mule.api.context.notification.ServerNotification;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.util.FileUtils;
 
@@ -27,29 +25,9 @@ public class UsingTheFileTransportToMoveFilesFromOneDirectoryToAnotherIT extends
         dataDirectory.mkdirs();
         new File("./data/expenses/1/in").mkdirs();
         new File("./data/expenses/out").mkdirs();
-        new File("./data/expenses/status").mkdirs();
+        
     }
-
-    @Override
-    protected void doSetUp() throws Exception {
-        super.doSetUp();
-        copyExpenseReportLatch = new CountDownLatch(1);
-        callbackLatch = new CountDownLatch(1);
-        muleContext.registerListener(new EndpointMessageNotificationListener() {
-            public void onNotification(final ServerNotification notification) {
-                if ("copyExpenseReports".equals(notification.getResourceIdentifier())
-                        && "receive".equals(notification.getActionName())) {
-                    copyExpenseReportLatch.countDown();
-                }
-
-                if ("expenseReportCallback".equals(notification.getResourceIdentifier())
-                        && "end dispatch".equals(notification.getActionName())) {
-                    callbackLatch.countDown();
-                }
-            }
-        });
-    }
-
+    
     @Override
     protected String getConfigResources() {
         return "using_the_file_transportto_move_files_from_one_directory_to_another.xml";
@@ -58,7 +36,7 @@ public class UsingTheFileTransportToMoveFilesFromOneDirectoryToAnotherIT extends
     @Test
     public void testCanCopyExpenseReports() throws Exception {
         FileUtils.writeStringToFile(new File("./data/expenses/1/in", "expenses.xls"), "a crazy bar tab");
-        Collection files = FileUtils.listFiles(new File("./data/expenses"), new String[]{"xls"}, true);
+        Collection<File> files = FileUtils.listFiles(new File("./data/expenses"), new String[]{"xls"}, true);
         Thread.sleep(5000);
         assertEquals(1, files.size());
     }
